4ab828a63aa80e70d14e9f1e2f8126b701098ed3
totaldekadens <angelica.moberg@hotmail.com>
2023-07-04 15:24:22 +0200

    Small fix

--- components/article/articleView/ArticleView.tsx
+++ components/article/articleView/ArticleView.tsx
@@ -26,7 +26,7 @@ const ArticleView = ({ article, setOpen }: Props) => {
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl px-4 sm:px-6  lg:max-w-7xl lg:px-8">
+        <div className="mx-auto max-w-2xl px-4 sm:px-6  lg:max-w-7xl lg:px-8 pt-6 sm:pt-0">
           <div className={cn(s.root, "fit")}>
             <div className={cn(s.main, "fit")}>
               <div className={s.sliderContainer}>
