ace3f2dacdc9ab97f17a86fe398d43d2744b8f05
totaldekadens <angelica.moberg@hotmail.com>
2023-07-08 13:09:48 +0200

    Fixed stats

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -3,47 +3,58 @@ import {
   IconPackages,
   IconServer2,
 } from "@tabler/icons-react";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { inventoryLocationContext } from "./context/InventoryLocationProvider";
+import { articleContext } from "./context/ArticleProvider";
 
 const Stats = () => {
   const { inventoryLocations } = useContext(inventoryLocationContext);
-  // Total amount of articles
-  /*   let totalAmount = currentArticles
-    ? currentArticles.reduce((sum, item) => sum + item.qty, 0)
-    : null; */
+  const { currentArticles } = useContext(articleContext);
 
-  /*   let totalSum = currentArticles?.reduce(
-    (sum, item) => sum + (item.price ? item.price : 0) * item.qty,
+  // Gets total qty of articles in stock. Couldn't do reduce right away.
+  const getQty = currentArticles.map((article) => Number(article.qty));
+  const totalQty = getQty.reduce((sum, item) => sum + item, 0);
+
+  // Gets total sales price of articles in stock. Couldn't do reduce right away.
+  const filterSales = currentArticles.filter((article) => article.price);
+  const newArray = filterSales.map((article) => ({
+    qty: article.qty,
+    price: article.price ? article.price : 0,
+  }));
+  const getTotalSales = newArray.reduce(
+    (sum, item) => sum + item.price * item.qty,
     0
-  ); */
+  );
+
   return (
     <div className="flex flex-col sm:flex-row items-center gap-6 mt-20">
-      <div className="flex-1 w-full sm:w-auto text-gray-800  justify-center flex items-center flex-col p-7 gap-4">
-        <IconPackages width={38} height={38} className="text-[#264133]" />{" "}
-        <p>Du har lagt in totalt:</p>
-        <p className="font-semibold text-xl ">
-          {/* totalAmount ? totalAmount : */} 5 produkter
-        </p>
-      </div>
-      <div className="flex-1 w-full sm:w-auto  text-gray-800   justify-center flex items-center flex-col p-7 gap-4">
-        <IconServer2 width={38} height={38} className="text-[#264133]" />
-        <p>Du har totalt:</p>
+      {currentArticles.length > 0 ? (
+        <>
+          <div className="flex-1 w-full sm:w-auto text-gray-800  justify-center flex items-center flex-col p-7 gap-4">
+            <IconPackages width={38} height={38} className="text-[#264133]" />{" "}
+            <p>Du har lagt in totalt:</p>
+            <p className="font-semibold text-xl ">{totalQty} produkter</p>
+          </div>
+          <div className="flex-1 w-full sm:w-auto  text-gray-800   justify-center flex items-center flex-col p-7 gap-4">
+            <IconServer2 width={38} height={38} className="text-[#264133]" />
+            <p>Du har totalt:</p>
 
-        <p className="font-semibold text-xl ">
-          {inventoryLocations?.length} st lagerplatser
-        </p>
-      </div>
-      <div className="flex-1 w-full sm:w-auto  text-gray-800  justify-center flex items-center flex-col p-7 gap-4">
-        <IconCurrencyKroneSwedish
-          width={38}
-          height={38}
-          className="text-[#264133]"
-        />
-        <p>Du har ett försäljningsvärde på totalt:</p>
+            <p className="font-semibold text-xl ">
+              {inventoryLocations?.length} st lagerplatser
+            </p>
+          </div>
+          <div className="flex-1 w-full sm:w-auto  text-gray-800  justify-center flex items-center flex-col p-7 gap-4">
+            <IconCurrencyKroneSwedish
+              width={38}
+              height={38}
+              className="text-[#264133]"
+            />
+            <p>Du har ett försäljningsvärde på totalt:</p>
 
-        <p className="font-semibold text-xl ">{/* {totalSum} */} 499 kr</p>
-      </div>
+            <p className="font-semibold text-xl ">{getTotalSales} kr</p>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
--- components/Table.tsx
+++ components/Table.tsx
@@ -29,7 +29,6 @@ const Table = () => {
   const [currentArticle, setCurrentArticle] =
     useState<PopulatedArticleDocument>();
 
-  console.log(currentArticles);
   return (
     <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 ">
       <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8 ">
