0cc605edcb8223a764f18b554a5a4939ef043e38
totaldekadens <angelica.moberg@hotmail.com>
2026-05-14 14:30:58 +0200

    Fixed pagination on image slider

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,4 +1,4 @@
-import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import Image from "next/image";
 
@@ -8,31 +8,42 @@ interface Props {
 
 const splideOptions = {
   arrows: false,
+  classes: {
+    pagination: "splide__pagination flex justify-center",
+    page: "splide__pagination__page !bg-gray-400 !opacity-100 [&.is-active]:!bg-gray-900",
+  },
 };
 
 const Slider = ({ images }: Props) => {
   return (
-    <Splide options={splideOptions} aria-label="My Favorite Images">
-      {images.map((image, i) => {
-        const path = `https://res.cloudinary.com/dkzh2lxon/image/upload/w_600/q_60/v1688383484/inventory/${image}`;
-        return (
-          <SplideSlide
-            key={i}
-            style={{ height: "100%" }}
-            className="flex justify-center"
-          >
-            <Image
-              src={path}
-              alt={"Bild på artikel"}
-              width={600}
-              height={600}
-              priority={i === 0}
-              quality="60"
-              className="object-contain h-full max-h-full"
-            />
-          </SplideSlide>
-        );
-      })}
+    <Splide
+      hasTrack={false}
+      options={splideOptions}
+      aria-label="My Favorite Images"
+    >
+      <SplideTrack className="mb-3">
+        {images.map((image, i) => {
+          const path = `https://res.cloudinary.com/dkzh2lxon/image/upload/w_600/q_60/v1688383484/inventory/${image}`;
+          return (
+            <SplideSlide
+              key={i}
+              style={{ height: "100%" }}
+              className="flex justify-center"
+            >
+              <Image
+                src={path}
+                alt={"Bild på artikel"}
+                width={600}
+                height={600}
+                priority={i === 0}
+                quality={60}
+                className="object-contain h-full max-h-full"
+              />
+            </SplideSlide>
+          );
+        })}
+      </SplideTrack>
+      <ul className="splide__pagination !static mt-6" />
     </Splide>
   );
 };
