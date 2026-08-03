0a17d69f5996afc304fcab164657fcb5094b152c
totaldekadens <angelica.moberg@hotmail.com>
2026-08-03 20:31:41 +0200

    small thing

--- components/searchbars/SearchBar.tsx
+++ components/searchbars/SearchBar.tsx
@@ -17,7 +17,7 @@ const SearchBar = ({ query, setQuery }: Props) => {
           "py-3.5 placeholder:text-gray-500",
           "block w-full rounded-md border-0 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 md:h-auto",
         )}
-        placeholder="Sök på valfri text..."
+        placeholder="Sök efter artikel..."
         onChange={(event) => setQuery(event.currentTarget.value)}
       />
     </div>
